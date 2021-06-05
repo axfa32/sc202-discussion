a6097927bef7ed790bcd37d17d1c7e1da202619d
axfa32 <frankhuang11@gmail.com>
2021-06-05 08:51:13 +0800

    Update 26.py

--- 26.py
+++ 26.py
@@ -8,4 +8,25 @@ class Solution:
                 i+=1
                 nums[i]=nums[j]
         return i+1   
-        # return sorted(set(nums)) 
+        # return sorted(set(nums)) class Solution:
+ class Solution2:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        numsLength = len(nums)
+        
+        if numsLength == 0:
+            return 0
+        
+        left = 0
+        right = 0
+        output = 1
+        
+        while right < numsLength:
+            if nums[left] == nums[right]:
+                right += 1
+            else:
+                left += 1
+                output += 1
+                nums[left] = nums[right]
+                
+        return output
+    
