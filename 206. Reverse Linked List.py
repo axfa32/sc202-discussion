b19b084081dce1b08209c648773177197f2acc2a
axfa32 <frankhuang11@gmail.com>
2021-06-09 14:56:19 +0800

    Create 206. Reverse Linked List.py

---
+++ 206. Reverse Linked List.py
@@ -0,0 +1,27 @@
+#iteration
+class Solution:
+# @param {ListNode} head
+# @return {ListNode}
+def reverseList(self, head):
+    prev = None
+    while head:
+        curr = head
+        head = head.next
+        curr.next = prev
+        prev = curr
+    return prev
+
+#Recursion
+
+class Solution:
+# @param {ListNode} head
+# @return {ListNode}
+def reverseList(self, head):
+    return self._reverse(head)
+
+def _reverse(self, node, prev=None):
+    if not node:
+        return prev
+    n = node.next
+    node.next = prev
+    return self._reverse(n, node)
