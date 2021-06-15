02f4525b709923efd8d4a532f998eedfc7fdf762
axfa32 <frankhuang11@gmail.com>
2021-06-15 13:14:30 +0800

    Update 112.py

--- 112.py
+++ 112.py
@@ -1,51 +1,56 @@
 #DFS Recursively
-def hasPathSum1(self, root, sum):
-    res = []
-    self.dfs(root, sum, res)
-    return any(res)
-    
-def dfs(self, root, target, res):
-    if root:
-        if not root.left and not root.right and root.val == target:
-            res.append(True)
-        if root.left:
-            self.dfs(root.left, target-root.val, res)
-        if root.right:
-            self.dfs(root.right, target-root.val, res)
+class Solution:
+	def hasPathSum(self, root, sum):
+		    if not root:
+			    return False
+		    if not root.left and not root.right and root.val == sum:
+			    return True
+		    return self.hasPathSum(root.left, sum-root.val) or self.hasPathSum(root.right, sum-root.val)
 
-# DFS with stack
-def hasPathSum2(self, root, sum):
-    if not root:
+class Solution:
+	def hasPathSum(self, root, sum): #have a function with a path summing up to a sum
+	    res = [] #this is the stack that i'll append the results to
+	    self.dfs(root, sum, res) #call on the dfs function and pass in the root, the sum and the result array
+	    return any(res) #when the dfs fun finishes if there is true in the function
+	def dfs(self, root, target, res):
+	    if root:
+		if not root.left and not root.right and root.val == target: #if root.left and root.left==null and root.val==target 
+		    res.append(True) #we can achieve sum and we append true to the array
+		if root.left: #The any() function returns True if any element of an iterable is True. If not, any() returns Fals
+		    self.dfs(root.left, target-root.val, res)
+		if root.right:
+		    self.dfs(root.right, target-root.val, res)
+
+# BFS with stack
+class Solution:
+	# DFS with stack
+    def hasPathSum(self, root, sum):
+        if not root:
+            return False
+        stack = [(root, root.val)]
+        while stack:
+            curr, val = stack.pop()
+            if not curr.left and not curr.right and val == sum:
+                return True
+            if curr.right:
+                stack.append((curr.right, val+curr.right.val))
+            if curr.left:
+                stack.append((curr.left, val+curr.left.val))
         return False
-    stack = [(root, root.val)]
-    while stack:
-        curr, val = stack.pop()
-        if not curr.left and not curr.right and val == sum:
-            return True
-        if curr.right:
-            stack.append((curr.right, val+curr.right.val))
-        if curr.left:
-            stack.append((curr.left, val+curr.left.val))
-    return False
-    
 # BFS with queue
-def hasPathSum(self, root, sum):
-    if not root:
-        return False
-    queue = [(root, sum-root.val)]
-    while queue:
-        curr, val = queue.pop(0)
-        if not curr.left and not curr.right and val == 0:
-            return True
-        if curr.left:
-            queue.append((curr.left, val-curr.left.val))
-        if curr.right:
-            queue.append((curr.right, val-curr.right.val))
-    return False
+class Solution:
+	def hasPathSum(self, root, sum):
+	    if not root:
+		return False
+	    queue = [(root, sum-root.val)]
+	    while queue:
+		curr, val = queue.pop(0)
+		if not curr.left and not curr.right and val == 0:
+		    return True
+		if curr.left:
+		    queue.append((curr.left, val-curr.left.val))
+		if curr.right:
+		    queue.append((curr.right, val-curr.right.val))
+	    return False
+
 	
-def hasPathSum1(self, root, sum):
-    if not root:
-        return False
-    if not root.left and not root.right and root.val == sum:
-        return True
-    return self.hasPathSum(root.left, sum-root.val) or self.hasPathSum(root.right, sum-root.val)
