b87734d43a058388a546285cdcf244d2f375ea13
axfa32 <frankhuang11@gmail.com>
2021-08-02 10:31:49 +0800

    Update whiteboard.py

--- whiteboard.py
+++ whiteboard.py
@@ -1,14 +1,14 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         result = []
-        def helper(lst):
+        def helper(s, i, currstr):
             if i == len(s):
-                result.append("".copylist)
+                result.append(currstr)
             else:
                 if s[i].isalpha():
-                    helper(s[i+1].lower())
-                    helper(s[i+1].upper())
+                    helper(s, i+1, currstr+s[i].lower())
+                    helper(s, i+1, currstr+s[i].upper())
                 else:
                     helper(s, i+1, currstr+s[i])
-            helper(list(s))
+            helper(s, 0, "")
             return result
